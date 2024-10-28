d9f2bc5fc261c1b7ab7df22ad5a84e3c401d7a79
Gregory Damiani <9481493+grggls@users.noreply.github.com>
2024-10-28 18:07:17 +0100

    remove ts jest file

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,6 +1,8 @@
-export default {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
     moduleFileExtensions: ['js', 'ts'],
-  };  
+};
